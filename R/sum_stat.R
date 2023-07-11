7f2677eb16d99b7366a74ab98136addb53c6a6a3
Steffan Christiansen <steffan.christiansen@sund.ku.dk>
2023-07-11 10:31:43 +0200

    sum_stat() added

---
+++ R/sum_stat.R
@@ -0,0 +1,34 @@
+
+#' Summary statistics
+#'
+#' @param df
+#' @param variables
+#'
+#' @return Summary statistics of selected variables
+#' @import dplyr
+#' @export
+#'
+#' @description sum_stat returns summary statistics of selected variables
+#' from a data frame
+#'
+#'
+#' @examples
+#'var <- c("mpg", "cyl")
+#'sum_stat(mtcars, var)
+
+sum_stat <- function(df, variables) {
+  measures <- c("minimum", "median", "maximum")
+  summaries <- bind_rows(
+    df %>%
+      select(variables) |>
+      summarise_all(min),
+    df %>%
+      select(variables) |>
+      summarise_all(median),
+    df %>%
+      select(variables) |>
+      summarise_all(max)
+  )
+  bind_cols(data.frame(value = measures, summaries))
+}
+

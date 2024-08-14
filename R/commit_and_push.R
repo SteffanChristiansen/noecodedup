5ff7330e6d642edf81b943ee850d8c1c891c55b4
SteffanChristiansen <snc@hst.aau.dk>
2024-08-14 08:49:34 +0200

    commit_and_push

---
+++ R/commit_and_push.R
@@ -0,0 +1,10 @@
+commit_message <- paste("Automated commit", date())
+
+# Stage all changes
+system("git add .")
+
+# Commit the changes with the provided commit message
+system(paste("git commit -m", shQuote(commit_message)))
+
+# Push the changes to the remote repository
+system("git push origin main")
